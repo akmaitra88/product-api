d4ce6efaa071afde3d235d2f50a3e51fd309f105
Arup Maitra <arup.maitra@accenture.com>
2024-03-05 00:55:21 +0000

    Updated Swagger UI settings

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -11,7 +11,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API V1");
+    c.RoutePrefix = "";
+});
 
 app.UseHttpsRedirection();
 
